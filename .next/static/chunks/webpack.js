90f7c56289b51f6d7408f84544e19c8da72b5ee5
Gilbert Gonzales <gigonz@extendhealth.com>
2022-12-30 15:45:12 -0700

    added something functions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6042799e1ba1cb7"; }
+/******/ 		__webpack_require__.h = function() { return "69422fe221ff3686"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/EndGameButton.js
+++ pages/EndGameButton.js
@@ -1,9 +1,14 @@
 import react from "react";
 
 function EndGameButton() {
+
+    function something4() {
+
+    }
+
     return (
         <div className="endgame-button" style={{ textAlign: "center" }}>
-            <button className="endgame" style={{ width: "150px", height: "75px", margin: "auto" }} onClick={() => (HideEndGameButton())}>End Game?</button>
+            <button className="endgame" style={{ width: "150px", height: "75px", margin: "auto" }} onClick={() => (something4())}>End Game?</button>
         </div>
 
     )
